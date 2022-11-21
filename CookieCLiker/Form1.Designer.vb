5ce4eb2c8840a7e1f5cbda1d91a518fc55e3efc5
Olafo0 <opaladzs@gmail.com>
2022-11-21 16:00:16 +0000

    Add project files.

---
+++ CookieCLiker/Form1.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Click_bt = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.NoobUPG = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Click_bt
+        '
+        Me.Click_bt.Location = New System.Drawing.Point(75, 210)
+        Me.Click_bt.Name = "Click_bt"
+        Me.Click_bt.Size = New System.Drawing.Size(132, 69)
+        Me.Click_bt.TabIndex = 0
+        Me.Click_bt.Text = "Click"
+        Me.Click_bt.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(132, 151)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(75, 23)
+        Me.TextBox1.TabIndex = 1
+        '
+        'NoobUPG
+        '
+        Me.NoobUPG.Location = New System.Drawing.Point(518, 53)
+        Me.NoobUPG.Name = "NoobUPG"
+        Me.NoobUPG.Size = New System.Drawing.Size(155, 23)
+        Me.NoobUPG.TabIndex = 2
+        Me.NoobUPG.Text = "Noob Upgrade"
+        Me.NoobUPG.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(152, 298)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(55, 23)
+        Me.TextBox2.TabIndex = 3
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(518, 97)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(155, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Grandma"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.NoobUPG)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Click_bt)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Click_bt As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents NoobUPG As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+End Class
---
+++ CookieCLiker/Form1.vb
@@ -0,0 +1,41 @@
+﻿Public Class Form1
+    Dim clicks_score As Integer
+    Dim CPS As Integer = 1
+    Dim Noob_B As Integer
+    Dim Grandma_B As Integer
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Click_bt.Click
+        clicks_score = clicks_score + CPS
+
+        TextBox1.Text = clicks_score
+        TextBox2.Text = CPS
+    End Sub
+
+    Private Sub NoobUPG_Click(sender As Object, e As EventArgs) Handles NoobUPG.Click
+        If Noob_B = 4 Then
+            NoobUPG.Hide()
+        Else
+            If clicks_score >= 150 Then
+                CPS = CPS + 25
+                clicks_score = clicks_score - 100
+                TextBox1.Text = clicks_score
+                TextBox2.Text = CPS
+                Noob_B = Noob_B + 1
+            End If
+        End If
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        If Grandma_B = 4 Then
+            Button1.Hide()
+        Else
+            If clicks_score >= 300 Then
+                CPS = CPS + 50
+                clicks_score = clicks_score - 150
+                TextBox1.Text = clicks_score
+                TextBox2.Text = CPS
+                Grandma_B = Grandma_B + 1
+            End If
+        End If
+    End Sub
+End Class
